03da44c8afa092651623a01d117bd58d33de2c0a
Juan Tobón Villa <jujotovil@gmail.com>
2021-10-28 19:33:19 -0500

    deployment changes

--- Transport_project/settings.py
+++ Transport_project/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import django_heroku
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,3 +157,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Transport_apps.newUser'
+
+
+django_heroku.settings(locals())
