7093332ae245a2790999ae85bdb57a265bafefc8
Juan Tobón Villa <jujotovil@gmail.com>
2021-10-29 07:06:41 -0500

    modifications

--- Transport_apps/admin.py
+++ Transport_apps/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.contrib import admin
 
 # Register your models here.
-from .models.newUsers import NewUser
 from .models.rutas import Ruta
 from .models.reservas import Reserva
 from .models.vehiculos import Vehiculo 
@@ -12,6 +11,6 @@ from .models.vehiculos import Vehiculo
 admin.site.register(Ruta)
 admin.site.register(Reserva)
 admin.site.register(Vehiculo)
-admin.site.register(NewUser)
+
 
 
