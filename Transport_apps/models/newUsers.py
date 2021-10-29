213678a24b63073d024a016a5f97b904c08c3d5f
Juan Tobón Villa <jujotovil@gmail.com>
2021-10-28 22:59:46 -0500

    Deployment changes

--- Transport_apps/models/newUsers.py
+++ Transport_apps/models/newUsers.py
@@ -4,19 +4,19 @@ from django.contrib.auth.hashers import make_password
 
 class UserManager(BaseUserManager):
    
-    def create_user(self, email, user_name, first_name, password, **other_fields):
+    def create_user(self, email, first_name, password, **other_fields):
 
         if not email:
             raise ValueError('El usuario debe tener un Email')
 
         email = self.normalize_email(email) 
-        user = self.model(email=email, user_name = user_name, first_name=first_name, **other_fields)
+        user = self.model(email=email, first_name=first_name, **other_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, user_name, first_name, password, **other_fields):
-        user = self.create_user(email, user_name, first_name, password)
+    def create_superuser(self, email, first_name, password, **other_fields):
+        user = self.create_user(email, first_name, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -24,13 +24,12 @@ class UserManager(BaseUserManager):
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(primary_key=True,unique=True)
-    user_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     is_staff = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+    REQUIRED_FIELDS = ['first_name']
     objects = UserManager()
 
     def __str__(self):
