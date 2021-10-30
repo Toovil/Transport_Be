d5114e0b1f5b3c537fd29d7fd7bed74adf5f7f2a
Juan Tobón Villa <jujotovil@gmail.com>
2021-10-29 23:33:10 -0500

    Implementación list

--- Transport_apps/views/userlistView.py
+++ Transport_apps/views/userlistView.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from Transport_apps.models import NewUser
 from Transport_apps.serializers import User_Serializer
 
@@ -7,4 +7,4 @@ from Transport_apps.serializers import User_Serializer
 class UserListView(generics.ListAPIView):
     queryset = NewUser.objects.all()
     serializer_class = User_Serializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny)
